254148fa27bef38aa56ed167b056d609ed08d5cf
Tharindu Jayawardhana <tharinduimalkajayawardhana@gmail.com>
2025-08-19 10:32:38 +0530

    Fix database role assignment in init-mongo script for reward service

--- reward-service/init-mongo.js
+++ reward-service/init-mongo.js
@@ -3,5 +3,5 @@ db = db.getSiblingDB('reward-service-db');
 db.createUser({
   user: 'reward-service-db-user',
   pwd: 'dbinitPASSWORD001axi00',
-  roles: [{ role: 'readWrite', db: 'reward-service-db' }]
+  roles: [{ role: 'readWrite', db: 'reward-db' }],
 });
